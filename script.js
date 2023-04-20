cb9115a8d1727c7a0e6046c1d992a9d4df55551b
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-20 19:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
+let prices = document.querySelectorAll(".price");
+let table = document.querySelector("table");
+
+
 const getSum = () => {
 //Add your code here
-  
+  let sum = 0;
+  prices.forEach((price) =>{
+   sum+=  Number (price.innerText);
+  });
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  td.innerText = sum;
+  tr.appendChild(td);
+
+  table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
